115509a08f32a2f9251d4825b170e33667b3e6c7
AndoPatrik <andopatrik0@gmail.com>
2020-10-30 21:20:56 +0100

    ADD: Basic integration test for ActivateUser

---
+++ xUnit_General_Purpose_API/UsersControllerIntegrationTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using MindMap_General_Purpose_API;
+using System.Net;
+using System.Net.Http;
+using Xunit;
+
+namespace xUnit_General_Purpose_API
+{
+    public class UsersControllerIntegrationTests
+    {
+        private readonly HttpClient _client;
+
+        public UsersControllerIntegrationTests()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+            _client = appFactory.CreateClient(); // Set dev mongo env
+        }
+
+        [Fact]
+        public async void ActivateUser_ShouldReturnOk() 
+        {
+            var response = await _client.GetAsync("https://localhost:6001/api/usermanagement/activate/5f95c5d26f4799e209733061");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
